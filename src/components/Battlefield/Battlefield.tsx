ddf84f2c59715c0b838228d60e84497976bcf32e
DrEiZ3R <vanyax941@gmail.com>
2023-06-28 15:52:44 +0300

    feat: add battlefield

---
+++ src/components/Battlefield/Battlefield.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import s from "./Battlefield.module.css";
+import Team from "../Team/Team.tsx";
+import Unit from "../Unit/Unit.tsx";
+import images from "../../tools/images.ts";
+
+type PropsType = {}
+
+const Battlefield: React.FC<PropsType> = () => {
+    const [round, setRound] = useState(1);
+    const arr = [
+        <Unit name="Skeleton" src={images.skeleton}/>,
+        <Unit name="Centaur" src={images.centaur}/>,
+        <Unit name="Bandit" src={images.bandit}/>,
+        <Unit name="Elf Archer" src={images.elfArcher}/>,
+        <Unit name="Skeleton mage" src={images.skeletonMage}/>,
+        <Unit name="Archimage" src={images.archimage}/>,
+        <Unit name="Monk" src={images.monk}/>,
+        <Unit name="Bishop" src={images.bishop}/>,
+        <Unit name="Sirena" src={images.siren}/>
+    ];
+
+    return (
+        <div className={s.wrapper}>
+            <Team color="red" units={arr.slice(0, 6)}/>
+            <div className={s.middle}>
+                <div className={s.round}>Round {round}</div>
+                <div className={s.vs}>VS</div>
+                <button className={s.defend}>Defend</button>
+            </div>
+            <Team color="green" units={arr.slice(3, 9)}/>
+        </div>
+    );
+};
+
+export default Battlefield;
