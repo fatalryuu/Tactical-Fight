f9cde248a2c0f4db3f7a6b8bd9ce8a513414a2c9
DrEiZ3R <vanyax941@gmail.com>
2023-06-29 20:57:58 +0300

    feat: add mass heal behavior

--- src/components/Battlefield/Team/Unit/Unit.tsx
+++ src/components/Battlefield/Team/Unit/Unit.tsx
@@ -42,9 +42,15 @@ const Unit: React.FC<PropsType> = ({ instance, currTeam, setCurrTeam, queue, ite
     const handleAction = () => {
         if (instance.status !== "dead") {
             if (queue[iterator].unitType === Type.HEALER_SINGLE || queue[iterator].unitType === Type.HEALER_MASS) {
-                queue[iterator].behavior.do(queue[iterator], instance);
-                setIterator(prev => prev + 1);
-                currTeam ? setCurrTeam(0) : setCurrTeam(1);
+                if (queue[iterator].unitType === Type.HEALER_SINGLE) {
+                    queue[iterator].behavior.do(queue[iterator], instance);
+                    setIterator(prev => prev + 1);
+                    currTeam ? setCurrTeam(0) : setCurrTeam(1);
+                } else {
+                    queue[iterator].behavior.do(queue[iterator], instance, queue);
+                    setIterator(prev => prev + 1);
+                    currTeam ? setCurrTeam(0) : setCurrTeam(1);
+                }
             } else if (team !== currTeam) {
                 queue[iterator].behavior.do(queue[iterator], instance);
                 setIterator(prev => prev + 1);
--- src/services/behaviors/MassHealBehavior.ts
+++ src/services/behaviors/MassHealBehavior.ts
@@ -3,6 +3,10 @@ import Unit from "../units/Unit.ts";
 
 export default class MassHealBehavior implements Behavior {
     do(attacker: Unit, target: Unit, units: Array<Unit>): void {
-        target.setCurrHP(attacker.damage);
+        if (attacker.team === target.team) {
+            units.filter((unit: Unit) => unit.team === attacker.team).forEach((unit: Unit) => unit.setCurrHP(-unit.maxHP));
+        } else {
+            target.setCurrHP(attacker.damage)
+        }
     }
 }
